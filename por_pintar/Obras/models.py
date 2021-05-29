2a9a84b5a77ffe58993471c0be924758ee315654
Manuel <manuel.garcia@unillanos.edu.co>
2021-05-28 19:27:11 -0500

    implementacion modelos en escena

--- por_pintar/Obras/models.py
+++ por_pintar/Obras/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Obras(models.Model):
-    model = models.FileField()
+    model = models.FileField(upload_to="objects3D/")
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to="imagenesGafas/")
     dimentions = models.CharField(max_length=50)
     date = models.DateField(verbose_name="Date Pices")
     price = models.CharField(max_length=150 )
