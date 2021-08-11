b4c6a6f67b23f55bf4c0d65ed498677f1250b422
Manuel  Garcia <manuelgarcia@iMac-de-Manuel.local>
2021-08-11 17:49:20 -0500

    sad

--- por_pintar/PorPintar/settings.py
+++ por_pintar/PorPintar/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'PorPintar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'juan_gallery',
+        'USER': 'manuel_garcia',
+        'PASSWORD': 'porpintar2021',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
